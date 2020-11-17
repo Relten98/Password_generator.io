5f3ae0d149e234029bde0ab65a02a055e1905729
Relten98 <73558735+Relten98@users.noreply.github.com>
2020-11-16 23:07:06 -0800

    Update 2
    Adjusted some more values for html, along with css sheet.
    
    Set groundwork for js password generator/condescending comment generator.

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,32 @@
-// Assignment Code
-var setLength = document.querySelector("#properlength");
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
+/// Sets a number value based on input from html
+var DesiredLength = document.getElementById("properlength").value;
 
-      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()1234567890",
-      retVal = "";
+/// Set of values for different responses for a randomized, condescending comment from the generator. 
+var snarky = ["Yeah, like you can memorize this.", "Good luck not forgetting this one.", "Not even the NSA is going to remember that password.", "Hopefully you have a pen handy", "Nothing screams 'secure' quite like a sticky note attached to the monitor filled with absolute gibberish... eh?", "Hopefully you aren't in a rush when have to input THAT password."]
+
+function snarkList(list){
+  return list[Math.floor(Math.random()* list.length)]; 
+}
+
+function snark() {
+var comment = snarkList(snarky);
+alert(comment)
+}
+
+//// Look ma, imma codin' like a champ
+
+function generatePassword() {
+  var length = DesiredLength,
+    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()1234567890",
+    retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+    retVal += charset.charAt(Math.floor(Math.random() * n));
   }
   return retVal;
 }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
