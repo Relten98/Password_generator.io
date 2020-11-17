ef50f35c1d4b70d15870604825590393cbf3ebb5
Relten98 <73558735+Relten98@users.noreply.github.com>
2020-11-16 23:11:08 -0800

    Update script.js

--- Develop/script.js
+++ Develop/script.js
@@ -19,7 +19,7 @@ alert(comment)
 
 function generatePassword() {
   var length = DesiredLength,
-    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()1234567890",
+    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*())_+=-';:/?.>,<~`,
     retVal = "";
   for (var i = 0, n = charset.length; i < length; ++i) {
     retVal += charset.charAt(Math.floor(Math.random() * n));
