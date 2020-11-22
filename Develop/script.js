85db16f0385a73319fa0042a2afbdb30900b09ff
Relten98 <73558735+Relten98@users.noreply.github.com>
2020-11-21 21:49:21 -0800

    FIGHTING THE CODE THAT BE
    At wits end, struggling with getting this to work.... regretting not asing more questions....

--- Develop/script.js
+++ Develop/script.js
@@ -1,30 +1,25 @@
 var generateBtn = document.querySelector("#generate");
-
-/// Sets a number value based on input from html
-var DesiredLength = document.getElementById("properlength").value;
-
-/// Set of values for different responses for a randomized, condescending comment from the generator. 
-var snarky = ["Yeah, like you can memorize this.", "Good luck not forgetting this one.", "Not even the NSA is going to remember that password.", "Hopefully you have a pen handy", "Nothing screams 'secure' quite like a sticky note attached to the monitor filled with absolute gibberish... eh?", "Hopefully you aren't in a rush when have to input THAT password."]
-
-function snarkList(list){
-  return list[Math.floor(Math.random()* list.length)]; 
-}
-
-function snark() {
-var comment = snarkList(snarky);
-alert(comment)
-}
+var x = document.getElementById("passlength").value;
 
 //// Look ma, imma codin' like a champ
 
 function generatePassword() {
-  var length = DesiredLength,
-    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*())_+=-';:/?.>,<~`,
+  /// WHY ISN'T THIS WORKING?!?!?!?!
+  console.log("My value is" + x);
+  charlist = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*())_+=-';:/?.>,<~`,"
+
+  var length = x,
+
     retVal = "";
-  for (var i = 0, n = charset.length; i < length; ++i) {
-    retVal += charset.charAt(Math.floor(Math.random() * n));
+
+  for (var i = 0, n = charlist.length; i < length; ++i) {
+
+    retVal += charlist.charAt(Math.floor(Math.random() * n));
+
   }
+
   return retVal;
+
 }
 
 // Write password to the #password input
