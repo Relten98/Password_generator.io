bca219f970649de7f281e4caaddc652bcb4c2872
Relten98 <73558735+Relten98@users.noreply.github.com>
2020-11-28 17:35:06 -0800

    Password generator functions
    fixed generator, now it properly generates a password depending on imput length, along with proper validation to ensure input follows a particular criteria.

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,16 @@
 var generateBtn = document.querySelector("#generate");
-var x = document.getElementById("passlength").value;
-
+let passLength = document.getElementById("passLength").value;
+var valid = false
+let x = 0
 //// Look ma, imma codin' like a champ
 
+
 function generatePassword() {
+  var length = x;
   /// WHY ISN'T THIS WORKING?!?!?!?!
-  console.log("My value is" + x);
   charlist = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*())_+=-';:/?.>,<~`,"
 
-  var length = x,
-
-    retVal = "";
+  retVal = "";
 
   for (var i = 0, n = charlist.length; i < length; ++i) {
 
@@ -22,14 +22,44 @@ function generatePassword() {
 
 }
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+function inputter() {
+  x = this.value;
+  console.log("My value is" + x);
+  return;
 }
 
+
+/// Checks to make sure password is valid.
+function validator() {
+  x = this.value;
+  console.log("My value is" + x);
+  if (x < 8) {
+    valid = false;
+    alert("Password cannot be shorter than 8 characters.");
+    return;
+  }
+  else if (x > 128) {
+    valid = false;
+    alert("Password cannot be longer than 128 characters");
+    return;
+  }
+  else {
+    valid = true;
+    return;
+  }
+}
+// Write password to the #password input
+function writePassword() {
+  if (valid == true) {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+  } else {
+    return;
+  }
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+document.getElementsByName("passLength")[0].addEventListener("change", validator);
