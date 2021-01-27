1e8b31fee08bd4825b8ef64f378e346a03dda3b7
Relten98 <diezhandinoty@gmail.com>
2021-01-26 22:35:21 -0800

    attempted to make js read alternative symbols

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,29 @@
 var generateBtn = document.querySelector("#generate");
 let passLength = document.getElementById("passLength").value;
+
+let numberSwitch = document.getElementById("numberSwitch");
+let symbolSwitch = document.getElementById("symbolSwitch");
 var valid = false
+
 let x = 0
 
+var useNumbers = false
+var useCharacters = false
+
 //// Look ma, imma codin' like a champ
+
+
 
 function generatePassword() {
   var length = x;
   /// HAHAHAHA TAKE THAT, BROKEN CODE!!!
-  charlist = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*())_+=-';:/?.>,<~`,"
+  charlist = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZc"
+  if (useCharacters = true) {
+    charlist + "!@#$%^&*())_+=-';:/?.>,<~`,"
+  }
+  if (useNumbers = true) {
+    charlist + "0123456789"
+}
 
   retVal = "";
 
@@ -63,3 +78,20 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 document.getElementsByName("passLength")[0].addEventListener("change", validator);
+symbolSwitch.addEventListener('change', function () {
+  if (checkbox.checked) {
+  var useCharacters = true
+    console.log('Numbers');
+  } else {
+    var useCharacters = true
+    console.log('No Numbers');
+  };
+
+numberSwitch.addEventListener('change', function () {
+if (checkbox.checked) {
+var useNumbers = true
+  console.log('Numbers');
+} else {
+  var useNumbers = true
+  console.log('No Numbers');
+}
